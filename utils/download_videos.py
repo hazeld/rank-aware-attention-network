aba3f275a77ca6d59198e9af009417c4a88931bc
Hazel Doughty <hd12584@bristol.ac.uk>
2019-05-30 17:02:55 +0100

    Added option to trim videos to segment used in 'The Pros and Cons' paper

--- utils/download_videos.py
+++ utils/download_videos.py
@@ -6,10 +6,25 @@ import pandas as pd
 parser = argparse.ArgumentParser('Program to download videos for BEST Dataset and EPIC-Skills')
 parser.add_argument('vid_list_filename', type=str)
 parser.add_argument('output_dir', type=str)
+parser.add_argument('--trim', default=False, action='store_true', help='Optionally trim the videos to the temporal bounds used in the paper')
 args = parser.parse_args()
 
 def read_vid_list(filename):
     return pd.read_csv(filename)
+
+def trim_videos(video_list_df, output_dir):
+    for i, row in video_list_df.iterrows():
+        if row['task'] == 'scrambled_eggs':
+            continue
+        vid_path = os.path.join(output_dir, row['task'], row['vid_id'] + '.mp4')
+        if not os.path.isfile(vid_path):
+            print('Video ' + row['vid_id'] + ' from task ' + row['task'] + ' not present, skipping')
+            continue
+        original_vid_path = vid_path.replace('.mp4', '_original.mp4')
+        os.rename(vid_path, original_vid_path)
+        duration = float(row['end_seconds']) - float(row['start_seconds'])
+        subprocess.call(['ffmpeg', '-ss', str(row['start_seconds']), '-i', original_vid_path, '-t', str(duration), '-c:a', 'ac3', '-c:v', 'libx264', '--', vid_path])
+        os.remove(original_vid_path)
 
 def download_vids(video_list_df, output_dir):
     errors = []
@@ -31,3 +46,5 @@ if __name__ == '__main__':
     errors = download_vids(video_list_df, args.output_dir)
     print('Unable download the following videos: ')
     print(errors)
+    if args.trim:
+        trim_videos(video_list_df, args.output_dir)
